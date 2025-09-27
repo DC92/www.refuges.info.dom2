ecea0edf9d909e0d05a3584baae9af36e994541c
Dominique Cavailhez <rando.gv92@gmail.com>
2025-09-27 20:42:41 +0200

    Mise au point 2 modes

--- vues/style.css.php
+++ vues/style.css.php
@@ -25,7 +25,6 @@ header('content-type: text/css');
 //Évitons que soit rechargée cette page à chaque coup, elle ne bouge pas beaucoup
 header('Cache-Control: max-age=86000');
 
-
 //Génération dynamique de certaines couleurs selon la saison
 $mois=date("n");
 if ($mois>9 and $mois<12)
@@ -62,12 +61,6 @@ switch ($periode)
     break;
 }
 
-function inv($couleur) {
-  $hex = '0123456789abcdef';
-  for($i = 0; $i < strlen($couleur); $i++)
-    $couleur[$i] = $hex[15 - strpos ($hex, strtolower($couleur[$i]))];
-  return $couleur;
-}
 ?>
 
 /* DOM 09/2025 on passe des constantes PHP aux constantes CSS */
@@ -76,30 +69,31 @@ function inv($couleur) {
   --couleur_titre: white;
   --couleur_lien: #<?=$couleur_lien?>;
   --couleur_fond: #<?=$couleur_fond?>;
-  --couleur_fond_clair: #<?=$couleur_fond?>;
-  --couleur_fond_sombre: #161210;
   --couleur_fond_titre: #<?=$couleur_lien?>;
-  --couleur_fond_amplifiee: #CEF99C;
+  --couleur_fond_amplifiee: #cef99c;
   --couleur_decoration_titres: #<?=$couleur_decoration_titres?>;
   --couleur_legende: #<?=$couleur_legende?>;
   --image_bandeau: url(../images/bandeau-haut/titrehorizontal_<?=date('m')?>.png);
 }
 
 /* Surcharge pour les pages du site en style sombre */
-.prosilver_dark,
-.ultra_light {
+.prosilver_dark {
   --couleur_texte: white;
   --couleur_titre: black;
   --couleur_lien: #<?=$couleur_lien_clair?>;
-  --couleur_fond: var(--couleur_fond_sombre);
+  --couleur_fond: #161210;
   --couleur_fond_amplifiee: #333;
-  --couleur_legende: #<?=inv($couleur_legende)?>;
+  /*
+  --couleur_fond_titre: #<?=$couleur_lien?>;
+  --couleur_decoration_titres: #<?=$couleur_decoration_titres?>;
+  --couleur_legende: #<?=($couleur_legende)?>;
+  */
 }
 
 /*==================================================================*/
-/* Le style du forum est dans style_forum.css                       */
+/* Le style du forum est maintenant dans style_forum.css            */
 /* Il n'est chargé que pour les pages du forum                      */
-/* Il est surchargé pour chaque style de forum                      */
+/* Il est surchargé pour les styles de forum # de prosilver         */
 /*==================================================================*/
 
 /*==================================================================*/
@@ -127,305 +121,12 @@ body {
   color: var(--couleur_texte);
 }
 
-.bandeau-haut,
-table, tr, td, th {
-  border-color: var(--couleur_texte) !important;
+.commentaire_metainfo {
+  color: black;
 }
 
 .couleur_fond_amplifiee {
   background-color: var(--couleur_fond_amplifiee);
-}
-
-input,
-select,
-textarea {
-  margin: 2px 0;
-  background-color: var(--couleur_titre);
-  color: var(--couleur_texte);
-}
-
-/*=====TEXTE=======*/
-strong {
-  /* Strong Emphasis: gras+rouge */
-  font-weight: bold;
-  color: #FF0000;
-}
-
-cite {
-  /* Citation: gras+droit */
-  font-weight: bold;
-  font-style: normal;
-}
-
-blockquote>div {
-  /* en particulier les citations forum */
-  border-left: 2px;
-}
-
-blockquote {
-  /* message forum et commentaires */
-  margin: 1em;
-  font-size: normal;
-}
-
-blockquote p:before {
-  /* message forum et commentaires */
-  content: open-quote;
-  font-size: xx-large;
-}
-
-blockquote p:after {
-  /* message forum et commentaires */
-  content: close-quote;
-  font-size: xx-large;
-  vertical-align: text-top;
-  /* pour pas que cette derniere quote decale la derniere ligne */
-}
-
-/*====== Titres =======*/
-
-/* Au 15/10/2024 le titre h1 n'est utilisé qu'une seule et unique fois dans tous le site, c'est sur les fiches, pour le nom du point */
-h1 {
-  /* Ce titre principal n'est pas plus gros ou gras que les autres, mais significativement, c'est le titre le plus important de la page */
-  font-weight: bold;
-  font-style: normal;
-  font-size: large;
-  margin-bottom: 3px;
-}
-
-/* h2 ? faut pas chercher à comprendre, on n'utilise le h2 à aucun endroit ! Si je n'avais que ça à faire, il faudrait tout décaller h5->h4->h3->h2 dans toutes les vues, et le wiki ! */
-
-/* à noter que h3 h4 et h5 sont également très utilisés dans le wiki, dont le contenu est dans la base */
-h3 {
-  /* titres de pages */
-  font-weight: bold;
-  font-style: normal;
-  font-size: large;
-  padding: 0em 2em;
-  margin: 0em;
-  text-align: center;
-  margin-bottom: 3px;
-  color: var(--couleur_titre);
-  background-color: var(--couleur_fond_titre);
-}
-
-h4 {
-  /* sous titres */
-  padding-top: 4px;
-  /* sous FF, la padding par defo est immense */
-  padding-bottom: 2px;
-  padding-left: 10px;
-  font-size: large;
-  margin: 0px;
-  /* sous FF, la padding par defo est immense */
-  border-bottom: 2px solid var(--couleur_decoration_titres);
-  border-left: 2px solid var(--couleur_decoration_titres);
-}
-
-h5 {
-  /* sou-sou titre, pour l'instant que dans les fiches de refuges */
-  border-bottom: thin solid var(--couleur_decoration_titres);
-  font-size: medium;
-  /* sinon H5 cest tout petit ... */
-  margin-top: 15px;
-  margin-bottom: 3px;
-  padding-left: 10px;
-}
-
-p {
-  margin-bottom: 1em;
-}
-
-/*===== Listes =====*/
-ul {
-  /* les listes , y compris news en page de garde */
-  list-style-type: none;
-  margin: 0px;
-  padding: 0px 0px 0px 0px;
-}
-
-dt {
-  /* listes, de definitions */
-  font-weight: bold;
-  margin-top: 4px;
-}
-
-.liste-wri>dd {
-  /* listes, de definitions */
-  margin-left: 1em;
-}
-
-dt>button {
-  font-size: 60%;
-  padding: 0px;
-}
-
-li {
-  margin-bottom: 3px;
-}
-
-/*====== Formulaires======*/
-/*
-  Utilisé pour le formulaire de création ou modification ainsi que l'ajout de commentaires.
-  J'aimerais pouvoir rendre un peu plus cohérent (factoriser) les style de la plupart des formulaires du site (zone modérateur, modif fiche, ajout commentaires)
-  mais ça demande un peu de tests, je (sly 2024) le fais au fûr et à mesure.
-*/
-
-/* Pour faire disparaitre un élément au bout de 5 secondes*/
-.fade-out {
-  opacity: 0; // l'état par défaut est d'être invisible
-  animation-iteration-count: 1;
-  animation: fade;
-  animation-duration: 8s;
-}
-@keyframes fade {
-  0% { opacity: 1; filter:alpha(opacity=100); } // première frame, 100% visible
-  100% { opacity: 0; filter:alpha(opacity=0); } // dernière, 100% invisible
-}
-
-form#form_point fieldset {
-  border: thin solid transparent;
-  /* pour les allergiques aux barrieres ;) */
-  padding: 0.75em 0;
-}
-
-form#form_point .textarea {
-  width: 100%;
-  min-width: 150px;
-  max-width: 1000px;
-  min-height: 10em;
-}
-
-form#form_point .textarea label,
-form#form_point .textarea textarea {
-  width: calc(100% - 7px);
-}
-
-form#form_point .booleen {
-  clear: left;
-  float: left;
-  width: calc(100% - 7px);
-  min-width: 450px;
-  max-width: calc(100% - 4px);
-  text-align: right;
-  padding: 1px;
-}
-
-form#form_point .booleen legend {
-  clear: left;
-  float: left;
-}
-
-form#form_point .booleen label {
-  clear: none;
-  float: none;
-  padding-left: 5px;
-}
-
-form#form_export label {
-  clear: none;
-  float: left;
-  width: 16em;
-}
-
-#form_export fieldset fieldset:hover {
-  /* deco sur le fieldset actif, pour bien le differencier des autres */
-  border: thin dotted var(--couleur_texte);
-}
-
-form.wri label {
-  /* sans la classe WRI, ca fait foirer le forum PHPBB , et oui */
-  clear: left;
-  float: left;
-}
-
-form label[title]:after,
-form legend[title]:after {
-  /* combine pour exclure OL , leurs label ne sont pas dans des form */
-  content: url(../images/tooltip.png);
-}
-
-fieldset fieldset {
-  /* moins de déco pour les fieldset imbriques */
-  float: left;
-  border: thin solid transparent;
-}
-
-form .champs_null_masque>input {
-  /* couleur de la case "champ null" et masquee par defo */
-  outline: red solid 2px;
-  float: left;
-  display: none;
-}
-
-form .champs_null_masque>input:checked+* input {
-  /* desactive les input qui suivent */
-  visibility: hidden;
-}
-
-form .champs_null_masque>label {
-  /* permet a la case de s'intercaler au bon endroit */
-  clear: none;
-}
-
-.input_en_ligne {
-  width: 26em;
-}
-
-.input_captcha_court {
-  width: 3em;
-}
-
-.textarea_large {
-  width: 99%;
-  min-width: 150px;
-  max-width: 1000px;
-  min-height: 10em;
-}
-
-/* Cas particulier pour OL qui a des labels non standards */
-div#switch_nav label {
-  float: none;
-  clear: none;
-}
-
-input[type=checkbox]:hover,
-input[type=radio]:hover {
-  box-shadow: 0px 0px 10px #1300ff;
-}
-
-/*==========divers=======*/
-img {
-  /* images sans bordures */
-  border: 0px;
-  margin: 0px;
-  padding: 0px;
-}
-
-/*=========liens==========*/
-a:hover {
-  /*met en valeur les liens qd on est dessus */
-  background-color: var(--couleur_legende);
-  text-decoration: none;
-}
-
-/*
-J'intègre également les class des liens du forum
-en gros je veux tout de la même couleur
-*/
-
-body:not(#phpbb) a,
-body:not(#phpbb) a:visited {
-  color: var(--couleur_lien);
-  /* en accord avec le thème du forum, et moins agressif */
-  text-decoration: none;
-}
-
-/*========= Erreurs ==========*/
-.erreur_saisie {
-  border: 2px double red;
-  background-color: yellow;
-  padding: 10px;
 }
 
 /*==================================================================*/
@@ -440,18 +141,12 @@ body:not(#phpbb) a:visited {
   font-family: Times New Roman;
 }
 
+.menu-haut ul {
+  list-style-type: none;
+}
+
 .menu-haut form li {
   margin: 5px 2px 10px 2px;
-}
-
-.choix_style_clair,
-.choix_style_clair:hover {
-  background: var(--couleur_fond_clair);
-}
-
-.choix_style_sombre,
-.choix_style_sombre:hover {
-  background: var(--couleur_fond_sombre);
 }
 
 /* Fenêtres larges */
@@ -733,29 +428,309 @@ body:not(#phpbb) a:visited {
   font-size: 16px;
 }
 
-/* c'est la liste en bas de page */
-.basdepage #racourcismenus {
-  clear: both;
-  border: dashed thin #096Ea1;
-  text-align: center;
-  margin: 0px;
-}
-
-.basdepage ul {
-  clear: both;
-  text-align: center;
-}
-
-.basdepage li {
-  display: inline;
-  margin-right: 2em;
-}
-
 .basdepage img,
 form {
   /* tout le bazar de pub de bas de page, en ligne! */
   display: inline;
   vertical-align: middle;
+}
+
+/*==================================================================*/
+/* Styles des pages hors forum                                      */
+/*==================================================================*/
+<?php if (!defined('IN_PHPBB')) { ?>
+
+/*=====TEXTE=======*/
+strong {
+  /* Strong Emphasis: gras+rouge */
+  font-weight: bold;
+  color: #FF0000;
+}
+
+cite {
+  /* Citation: gras+droit */
+  font-weight: bold;
+  font-style: normal;
+}
+
+blockquote>div {
+  /* en particulier les citations forum */
+  border-left: 2px;
+}
+
+blockquote {
+  /* message forum et commentaires */
+  margin: 1em;
+  font-size: normal;
+}
+
+blockquote p:before {
+  /* message forum et commentaires */
+  content: open-quote;
+  font-size: xx-large;
+}
+
+blockquote p:after {
+  /* message forum et commentaires */
+  content: close-quote;
+  font-size: xx-large;
+  vertical-align: text-top;
+  /* pour pas que cette derniere quote decale la derniere ligne */
+}
+
+/*====== Titres =======*/
+
+/* Au 15/10/2024 le titre h1 n'est utilisé qu'une seule et unique fois dans tous le site, c'est sur les fiches, pour le nom du point */
+h1 {
+  /* Ce titre principal n'est pas plus gros ou gras que les autres, mais significativement, c'est le titre le plus important de la page */
+  font-weight: bold;
+  font-style: normal;
+  font-size: large;
+  margin-bottom: 3px;
+}
+
+/* h2 ? faut pas chercher à comprendre, on n'utilise le h2 à aucun endroit ! Si je n'avais que ça à faire, il faudrait tout décaller h5->h4->h3->h2 dans toutes les vues, et le wiki ! */
+
+/* à noter que h3 h4 et h5 sont également très utilisés dans le wiki, dont le contenu est dans la base */
+h3 {
+  /* titres de pages */
+  font-weight: bold;
+  font-style: normal;
+  font-size: large;
+  padding: 0em 2em;
+  margin: 0em;
+  text-align: center;
+  margin-bottom: 3px;
+  color: white;
+  background-color: var(--couleur_fond_titre);
+}
+
+h4 {
+  /* sous titres */
+  padding-top: 4px;
+  /* sous FF, la padding par defo est immense */
+  padding-bottom: 2px;
+  padding-left: 10px;
+  font-size: large;
+  margin: 0px;
+  /* sous FF, la padding par defo est immense */
+  border-bottom: 2px solid var(--couleur_decoration_titres);
+  border-left: 2px solid var(--couleur_decoration_titres);
+}
+
+h5 {
+  /* sou-sou titre, pour l'instant que dans les fiches de refuges */
+  border-bottom: thin solid var(--couleur_decoration_titres);
+  font-size: medium;
+  /* sinon H5 cest tout petit ... */
+  margin-top: 15px;
+  margin-bottom: 3px;
+  padding-left: 10px;
+}
+
+p {
+  margin-bottom: 1em;
+}
+
+/*===== Listes =====*/
+ul {
+  /* les listes , y compris news en page de garde */
+  list-style-type: none;
+  margin: 0px;
+  padding: 0px 0px 0px 0px;
+}
+
+dt {
+  /* listes, de definitions */
+  font-weight: bold;
+  margin-top: 4px;
+}
+
+.liste-wri>dd {
+  /* listes, de definitions */
+  margin-left: 1em;
+}
+
+dt>button {
+  font-size: 60%;
+  padding: 0px;
+}
+
+li {
+  margin-bottom: 3px;
+}
+
+
+/*====== Formulaires======*/
+/*
+  Utilisé pour le formulaire de création ou modification ainsi que l'ajout de commentaires.
+  J'aimerais pouvoir rendre un peu plus cohérent (factoriser) les style de la plupart des formulaires du site (zone modérateur, modif fiche, ajout commentaires)
+  mais ça demande un peu de tests, je (sly 2024) le fais au fûr et à mesure.
+*/
+
+/* Pour faire disparaitre un élément au bout de 5 secondes*/
+.fade-out {
+  opacity: 0; /* l'état par défaut est d'être invisible */
+  animation-iteration-count: 1;
+  animation: fade;
+  animation-duration: 8s;
+}
+@keyframes fade {
+  0% { opacity: 1; filter:alpha(opacity=100); } /* première frame, 100% visible */
+  100% { opacity: 0; filter:alpha(opacity=0); } /* dernière, 100% invisible */
+}
+
+form#form_point fieldset {
+  border: thin solid transparent;
+  /* pour les allergiques aux barrieres ;) */
+  padding: 0.75em 0;
+}
+
+select,
+input,
+textarea {
+  color: var(--couleur_texte);
+  background: var(--couleur_titre);
+}
+
+form#form_point .textarea {
+  width: 100%;
+  min-width: 150px;
+  max-width: 1000px;
+  min-height: 10em;
+}
+
+form#form_point .textarea label,
+form#form_point .textarea textarea {
+  width: calc(100% - 7px);
+}
+
+form#form_point .booleen {
+  clear: left;
+  float: left;
+  width: calc(100% - 7px);
+  min-width: 450px;
+  max-width: calc(100% - 4px);
+  text-align: right;
+  padding: 1px;
+}
+
+form#form_point .booleen legend {
+  clear: left;
+  float: left;
+}
+
+form#form_point .booleen label {
+  clear: none;
+  float: none;
+  padding-left: 5px;
+}
+
+form#form_export label {
+  clear: none;
+  float: left;
+  width: 16em;
+}
+
+#form_export fieldset fieldset:hover {
+  /* deco sur le fieldset actif, pour bien le differencier des autres */
+  border: thin dotted var(--couleur_texte);
+}
+
+form.wri label {
+  /* sans la classe WRI, ca fait foirer le forum PHPBB , et oui */
+  clear: left;
+  float: left;
+}
+
+form label[title]:after,
+form legend[title]:after {
+  /* combine pour exclure OL , leurs label ne sont pas dans des form */
+  content: url(../images/tooltip.png);
+}
+
+fieldset fieldset {
+  /* moins de déco pour les fieldset imbriques */
+  float: left;
+  border: thin solid transparent;
+}
+
+form .champs_null_masque>input {
+  /* couleur de la case "champ null" et masquee par defo */
+  outline: red solid 2px;
+  float: left;
+  display: none;
+}
+
+form .champs_null_masque>input:checked+* input {
+  /* desactive les input qui suivent */
+  visibility: hidden;
+}
+
+form .champs_null_masque>label {
+  /* permet a la case de s'intercaler au bon endroit */
+  clear: none;
+}
+
+.input_en_ligne {
+  width: 26em;
+}
+
+.input_captcha_court {
+  width: 3em;
+}
+
+.textarea_large {
+  width: 99%;
+  min-width: 150px;
+  max-width: 1000px;
+  min-height: 10em;
+}
+
+/* Cas particulier pour OL qui a des labels non standards */
+div#switch_nav label {
+  float: none;
+  clear: none;
+}
+
+input[type=checkbox]:hover,
+input[type=radio]:hover {
+  box-shadow: 0px 0px 10px #1300ff;
+}
+
+/*==========divers=======*/
+img {
+  /* images sans bordures */
+  border: 0px;
+  margin: 0px;
+  padding: 0px;
+}
+
+/*=========liens==========*/
+a:hover {
+  /*met en valeur les liens qd on est dessus */
+  background-color: var(--couleur_legende);
+  text-decoration: none;
+}
+
+/*
+J'intègre également les class des liens du forum
+en gros je veux tout de la même couleur
+*/
+
+body:not(#phpbb) a,
+.bandeau-haut a,
+body:not(#phpbb) a:visited {
+  color: var(--couleur_lien);
+  /* en accord avec le thème du forum, et moins agressif */
+  text-decoration: none;
+}
+
+/*========= Erreurs ==========*/
+.erreur_saisie {
+  border: 2px double red;
+  background-color: yellow;
+  padding: 10px;
 }
 
 /*==================================================================*/
@@ -830,10 +805,11 @@ form {
   float: left;
   position: relative;
   max-width: 100%;
-  color: initial;
+  color: white;
 }
 
 .texte_sur_image {
+  color: white;
   text-shadow: 2px 0 #555, -2px 0 #555, 0 2px #555, 0 -2px #555,
     1px 1px #555, -1px -1px #555, 1px -1px #555, -1px 1px #555;
   position: absolute;
@@ -852,11 +828,6 @@ form {
   margin-inline-end: 0;
 }
 
-/* Lien pour retourner à la fiche depuis le forum */
-.wri-link {
-  font-size: 70%;
-}
-
 .traces,
 .point_forum em {
   color: var(--couleur_texte) !important;
@@ -873,7 +844,7 @@ form {
 }
 
 .ol-viewport * {
-  color: initial;
+  color: white;
 }
 
 /* Carte de l'accueil */
@@ -1106,3 +1077,6 @@ form {
   background-color: DodgerBlue !important;
   color: #ffffff;
 }
+
+/* Fin des styles des pages hors forum */
+<?php } ?>
